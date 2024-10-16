0d48344d24318376fddaef32161a8ad2b0bcdc61
Naif Otibi <nn.n@outlook.cl>
2024-10-16 21:51:45 +0300

    first appearence of switch

---
+++ src/app/components/Form.tsx
@@ -0,0 +1,10 @@
+"use client"
+
+
+
+
+export default function Form(){
+    return <div className="space-y-2">
+        <switch
+    </div>
+}
---
+++ src/app/components/Switch.tsx
@@ -0,0 +1,11 @@
+
+
+type SwitchProps = {
+    checked: boolean,
+    onCheckedChange: (checked: boolean) => void
+}
+
+
+export default function Switch({checked , onCheckedChange}: SwitchProps){
+    
+}
