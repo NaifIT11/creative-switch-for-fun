90f0fb01d7d6720c5e3a88ed39bbf40611c0c380
Naif Otibi <nn.n@outlook.cl>
2024-10-17 00:09:34 +0300

    finishing the logic

--- src/app/components/Switch.tsx
+++ src/app/components/Switch.tsx
@@ -1,16 +1,29 @@
-
+import { Angry, Smile } from "lucide-react";
 
 type SwitchProps = {
-    checked: boolean,
-    onCheckedChange: (checked: boolean) => void
-}
+  checked: boolean;
+  onCheckedChange: (checked: boolean) => void;
+};
 
-
-export default function Switch({checked , onCheckedChange}: SwitchProps){
-    const handleClick = (e: React.FormEvent<HTMLButtonElement>) => {
-        onCheckedChange(!checked);
-    }
-    return <button onClick={handleClick} data-checked={checked} className="data-[checked=true]:bg-green-300 w-[300px] h-[100px] rounded border rounded bg-gray-100" role="switch" aria-checked={checked} aria-label="Switch Control">
-        <div data-checked={checked} className="data-[checked=true]:translate-x-[198px] transition w-[100px] h-[100px] bg-white"></div>
+export default function Switch({ checked, onCheckedChange }: SwitchProps) {
+  const handleClick = (e: React.FormEvent<HTMLButtonElement>) => {
+    onCheckedChange(!checked);
+  };
+  return (
+    <button
+      onClick={handleClick}
+      data-checked={checked}
+      className="data-[checked=true]:bg-green-300 w-[300px] h-[100px] rounded border rounded bg-gray-100"
+      role="switch"
+      aria-checked={checked}
+      aria-label="Switch Control"
+    >
+      <div
+        data-checked={checked}
+        className="data-[checked=true]:translate-x-[198px] transition w-[100px] h-[100px] bg-white"
+      >
+        {checked ?   <Smile className="w-full h-full text-black" /> : <Angry className="w-full h-full text-gray-200" />}
+      </div>
     </button>
+  );
 }
