176bde1fd04a824e7f69206e9a248a5f89ecbef5
Naif Otibi <nn.n@outlook.cl>
2024-10-16 21:54:55 +0300

    adding form and centering

--- src/app/components/Form.tsx
+++ src/app/components/Form.tsx
@@ -1,10 +1,14 @@
 "use client"
 
+import { useState } from "react"
+import Switch from "./Switch"
+
 
 
 
 export default function Form(){
-    return <div className="space-y-2">
-        <switch
+    const [checked  , setChecked] = useState(false);
+    return <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <Switch checked={checked} onCheckedChange={setChecked} />
     </div>
 }
